fa42daf44b559915c031e560065d290c200d92cf
Sean O'Hearn <seohearn1@gmail.com>
2019-07-18 22:39:08 -0700

    initial commit for authentication api

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # cruzhacks-2020-authentication-microservice
-Authentication microservice validating user logins and requests
 
--- WIP --
+- work in progress
+  - this is a boilerplate for handling routes that will require authentication for the cruzhacks website
+- this backend service's purpose will be to validate tokens sent from the client
+  - further down the line it will also be used for authentication between APIs if needed
+
+I'm using express for azure functions since express is required to use auth0's node-js wrapper for creating a back-end authentication service
+
+Next I'm going to create a test client that will be used for development. Right now there's really no way for you to generate a token to test this service unless you want to create a test client yourself.
+
+The following link might help clarify what this service should accomplish, and it's where I got started with this iteration of the authentication service.
+
+https://auth0.com/docs/quickstart/backend/nodejs
---
+++ authenticate/index.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const createAzureFunctionHandler = require("azure-function-express")
+  .createHandler;
+const app = require("express")();
+const jwt = require("express-jwt");
+// const jwtAuthz = require("express-jwt-authz"); // we'll be using this for authorization between API's
+const jwksRsa = require("jwks-rsa");
+const cors = require("cors");
+require("dotenv").config();
+
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+  throw "Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file";
+}
+
+// using default react port
+const corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ["RS256"]
+});
+
+// placeholder for private endpoint user will be allowed to navigate to once authenticated
+app.get("/api/:authenticate/:private", checkJwt, function(req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated to see this."
+  });
+});
+
+// app.get("/private", checkJwt, function(req, res) {
+//   res.json({
+//     message:
+//       "Hello from a private endpoint! You need to be authenticated to see this."
+//   });
+// });
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  return res.status(err.status).json({ message: err.message });
+});
+
+module.exports = createAzureFunctionHandler(app);
